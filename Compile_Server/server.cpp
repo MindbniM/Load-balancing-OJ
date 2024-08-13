4fd7f897a9564923cfa87fd4e5749ad6bdce64b2
MindbniM <1771392560@qq.com>
2024-08-13 17:09:38 +0800

    Compile_Server端口设定修改

--- Compile_Server/server.cpp
+++ Compile_Server/server.cpp
@@ -2,11 +2,20 @@
 #include"Compile_run.hpp"
 #include "log.hpp"
 #include<httplib.h>
-int main()
+void Usage()
 {
+    std::cerr<<"\t ./C_server [port]"<<std::endl;
+}
+int main(int argc,char* argv[])
+{
+    if(argc!=2)
+    {
+        Usage();
+        return 0;
+    }
     ADD_APPENDER_STDOUT(Log_util::log_level::DEBUG);
     httplib::Server sr;
-    sr.Get("/Compile_Run",[](const httplib::Request& re,httplib::Response& rp)
+    sr.Post("/Compile_Run",[](const httplib::Request& re,httplib::Response& rp)
     {
         std::string jsonin=re.body;
         std::string jsonout;
@@ -16,6 +25,6 @@ int main()
             rp.set_content(jsonout,"application/json;chatset=utf-8");
         }
     });
-    sr.listen("0.0.0.0",8848);
+    sr.listen("0.0.0.0",atoi(argv[1]));
     return 0;
 }
